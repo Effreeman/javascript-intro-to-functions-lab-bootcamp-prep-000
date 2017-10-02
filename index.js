e623f09bf497aa5d5507f671494b9e98cfa70d16
Emma Freeman <freemoe04@gmail.com>
2017-10-02 19:35:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string) {
+function sayHiToGrandma('hello') {
   return ("I can't hear you!")
   if (true) {logWhisper(string)}
 }
-function sayHiToGrandma(string) {
+function sayHiToGrandma('HELLO') {
   return ("YES INDEED!")
   if (true) {logShout(string)}
 }
