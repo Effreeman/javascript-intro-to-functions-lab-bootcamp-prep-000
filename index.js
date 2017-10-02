58778803606fe25c841439766204adc90b6fadec
Emma Freeman <freemoe04@gmail.com>
2017-10-02 18:47:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,5 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   return ("I can't hear you!")
-  if (true) {shout(string)
-
-  } else {"YES INDEED!"
-  if (true) {whisper(string)
-  }
+  if (true) {shout(string)}
+}
