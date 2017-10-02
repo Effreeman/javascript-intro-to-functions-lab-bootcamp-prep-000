022aca28cfff71e1763acc89f8ff4d67189a70c5
Emma Freeman <freemoe04@gmail.com>
2017-10-02 19:59:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string) {
-  return ('hello')
+function sayHiToGrandma(hello) {
+  return ("I can't hear you")
 }
